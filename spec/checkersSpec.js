c0f71b555afe9e2f24e900de4c9a4f0d2fa71cc7
David Ogor <doctown@yahoo.com>
2016-04-04 16:37:53 -0700

    Add scripts for methods

--- spec/checkersSpec.js
+++ spec/checkersSpec.js
@@ -17,11 +17,14 @@ describe('checkers', function() {
     });
 
     beforeEach(function() {
+      piece = new Piece({color: red, cur_pos: {x: 0, y: 0}})
     });
 
     describe('piece', function() {
-      it('should do this', function() {
-        expect(true).to.equal(true);
+      it('should be set to initial color, position, and state', function() {
+        expect(this.get('color')).to.equal('red');
+        expect(this.get('state')).to.equal('single');
+        expect(this.get('cur_pos')).to.deep.equal({x: 0, y: 0});
       });
     });
 
